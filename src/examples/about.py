83dbea781d66bfbee4edeba5ffe86e94e6d846bb
Michael <michael@groene.de>
2026-06-28 21:13:50 +0100

    add log output to example

--- src/examples/about.py
+++ src/examples/about.py
@@ -1,6 +1,15 @@
+import logging
+import sys
+
 from rclone_api.api import RcloneApi
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.DEBUG,
+        format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
+    )
+
     rc = RcloneApi()
     rc.start()
 
