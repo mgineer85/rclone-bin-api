d979f35af3ab19bd8c2afcee5c000865c1590915
Michael G <michael@groene.de>
2026-08-05 07:41:33 +0200

    use httpx2 instead urllib so we can handle timeouts better in future and use more recent http protocol as well as improved name resolution

--- src/examples/about.py
+++ src/examples/about.py
@@ -6,7 +6,7 @@ from rclone_api.api import RcloneApi
 if __name__ == "__main__":
     logging.basicConfig(
         stream=sys.stdout,
-        level=logging.DEBUG,
+        level=logging.WARNING,
         format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
     )
 
